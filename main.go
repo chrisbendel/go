fdf4437532af099fe4f308ce424ad050e3c2b43a
Chris Bendel <chrissbendel@gmail.com>
2018-03-20 18:24:45 -0400

    stuff

--- main.go
+++ main.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"log"
+	"strconv"
+)
 
 func main() {
-	fmt.Printf("Hello, world.\n")
+	x, err := StrInverse("8")
+	if err != nil {
+		log.Printf("%s", err)
+		return
+	}
+	fmt.Println(x)
+
+}
+
+func foo(n int) (int, error) {
+	return 0, errors.New("Broken")
+}
+
+func test(a string) {
+	fmt.Print(a)
+}
+
+func StrInverse(n string) (float64, error) {
+	denom, err := strconv.ParseFloat(n, 64)
+	if err != nil {
+		return 0, fmt.Errorf("StrInver: %s %v", "Number is not a string", err)
+	}
+	if denom == 0 {
+		return 0, errors.New("Can't divide by zero")
+	}
+
+	return 1 / float64(denom), nil
 }
